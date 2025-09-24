71f06da6f4a8dc0275565a63190dd949a9dd5759
Manojkamriya <kamriyamanoj45@gmail.com>
2025-09-24 18:12:19 +0530

    vite.config.js updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{port:5173}
-})
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
+  server: {
+    port: 5173,       // only used for local dev
+  },
+  build: {
+    outDir: 'dist'    // Vercel expects 'dist', this is Vite default anyway
+  }
+});
